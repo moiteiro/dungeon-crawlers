02f7e3f1bb66bd7a72e997bb668596fc2a374098
Moiteiro <bruno.moiteiro@gmail.com>
2013-01-23 11:10:23 -0200

    Refs #1. Initail Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DungeonCrawlers::Application.config.secret_token = '3390f6651ba8bae314b0c7dfd4e75880d8e2535fc4d3e719dec1b6c0315dd04f35777fd6a71a224be577e6addb3a2e55c71e7b763f0c6c2e1b3e58b9e6082023'
